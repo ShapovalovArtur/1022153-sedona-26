36b3e16e0a072472b91b1e66a0c3b19a4dd23ca5
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2019-10-15 17:51:19 +0300

    js script in file added

---
+++ js/slide.js
@@ -0,0 +1,5 @@
+var popup = document.querySelector(".modal-book");
+var modalButton = document.querySelector(".modal-book-button");
+  modalButton.addEventListener("click", function () {
+  popup.classList.toggle("modal-hide");
+});
