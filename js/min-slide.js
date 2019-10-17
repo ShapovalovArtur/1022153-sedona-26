7b389a6f2ee28e084983b827d2d76cdb46c6ff50
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2019-10-17 15:34:15 +0300

    minificated

---
+++ js/min-slide.js
@@ -0,0 +1,1 @@
+var popup = document.querySelector(".modal-book"), modalButton = document.querySelector(".modal-book-button"); modalButton.addEventListener("click", function () { popup.classList.toggle("modal-hide") });
